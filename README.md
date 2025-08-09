07e536a9c3065179019a034ab9f72834129e2fa6
Chiranjeevi Naidu Chintalapudi <chiranjeevichintalapudi777@gmail.com>
2025-08-09 13:58:29 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# edutrack
-a application for tracking the students academics
+# Edutracker
+ Track your way to success with edutracker
+## Technologies Used -
+ ###### Back-end : Node  JS, Express JS
+ ###### Database : Mongo DB (Mongoose)
+ ###### Authentication : Passport JS
+ ###### Front-end : HTML, EJS, CSS, JS, Semantic UI, Bootstrap
+ ###### Machine Learning : Python (nltk : tokenizing ; python-pptx : reading pptx file ; numpy)
+
+ 
+## Installation :
+ #### Install python and the following libraries
+ ```bash
+ pip install nltk
+ pip install numpy
+ pip install python-pptx
+ ```
+ #### Install Nodejs and Mongo.
+ #### Clone the repository
+  ```bash
+  git clone https://github.com/SparshJain2000/edutracker.git
+  ```
+ #### Install node libraries
+  ```bash
+  npm install
+  ```
+ #### Declare environment variables
+ ###### Create a file .env
+  ```txt
+  MONGO_URI = 'Your mongo url'
+  SECRET = 'secret for Passport'
+  ```
+ #### Start the server (run app.js file)
+  ```bash
+  node app.js
+  ```
---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require("express"),
+    app = express(),
+    methodOverride = require("method-override"),
+    mongoose = require("mongoose"),
+    request = require("request"),
+    flash = require("connect-flash"),
+    bodyParser = require("body-parser"),
+    Student = require("./models/student"),
+    Faculty = require("./models/faculty"),
+    cors = require("cors"),
+    passport = require("passport"),
+    LocalStratergy = require("passport-local"),
+    env = require("dotenv");
+env.config();
+
+//===================================================================
+//Requiring ROUTES
+
+var indexRoutes = require("./routes/index"),
+    studentRoutes = require("./routes/student"),
+    filesRoutes = require("./routes/files"),
+    facultyRoutes = require("./routes/faculty");
+
+//===================================================================
+//Connecting mongoose to mongoDB
+
+mongoose
+    .connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((x) => console.log("Connected to mongo"));
+
+//===================================================================
+//linking external files (stylesheets and scripts)
+app.use(express.static(__dirname + "/public"));
+
+//for flash messages
+app.use(flash());
+
+//for post routes
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+//setting view engine ejs
+app.set("view engine", "ejs");
+
+//setting session
+app.use(
+    require("express-session")({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+    }),
+);
+
+//===================================================================
+//Passport initialization
+//multiple user type
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use("student", new LocalStratergy(Student.authenticate()));
+passport.use("faculty", new LocalStratergy(Faculty.authenticate()));
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user, done) => {
+    if (user != null) {
+        done(null, user);
+    }
+});
+
+//===================================================================
+//setting locals
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.err = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+//===================================================================
+//setting up routes
+
+app.use("/", indexRoutes);
+app.use("/student", studentRoutes);
+app.use("/faculty", facultyRoutes);
+app.use("/files", filesRoutes);
+
+//===================================================================
+//listening at a port
+
+const port = process.env.PORT || 7000;
+app.listen(port, () => console.log("listening at " + port));
+
+//===================================================================
